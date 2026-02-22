06b127825cb8ae1edc7cedc94718ab90cdd708e5
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2026-02-23 00:18:41 +0530

    update vim-test

--- nvim/.config/nvim/lua/plugins/vim-test.lua
+++ nvim/.config/nvim/lua/plugins/vim-test.lua
@@ -4,11 +4,11 @@ return {
 		"preservim/vimux", -- Runs tests in a tmux pane
 	},
 	config = function()
-		vim.keymap.set("n", "<leader>t", ":TestNearest<CR>", { desc = "Run nearest test" })
-		vim.keymap.set("n", "<leader>T", ":TestFile<CR>", { desc = "Run all tests in file" })
-		vim.keymap.set("n", "<leader>a", ":TestSuite<CR>", { desc = "Run entire test suite" })
-		vim.keymap.set("n", "<leader>l", ":TestLast<CR>", { desc = "Re-run last test" })
-		vim.keymap.set("n", "<leader>g", ":TestVisit<CR>", { desc = "Jump to last test file" })
+		vim.keymap.set("n", "<leader>tn", ":TestNearest<CR>", { desc = "Run nearest test" })
+		vim.keymap.set("n", "<leader>tf", ":TestFile<CR>", { desc = "Run all tests in file" })
+		vim.keymap.set("n", "<leader>ta", ":TestSuite<CR>", { desc = "Run entire test suite" })
+		vim.keymap.set("n", "<leader>tl", ":TestLast<CR>", { desc = "Re-run last test" })
+		vim.keymap.set("n", "<leader>tg", ":TestVisit<CR>", { desc = "Jump to last test file" })
 
 		vim.cmd("let test#strategy = 'vimux'")
 	end,
