924e9bdd35eb06f080816f8d4b1f86b37ec96c81
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2026-03-06 03:33:11 +0530

    update bufferline config

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -1,34 +1,74 @@
 return {
 	{
 		"akinsho/bufferline.nvim",
-		dependencies = "nvim-tree/nvim-web-devicons",
+		dependencies = {
+			{ "nvim-tree/nvim-web-devicons", opts = {} },
+			"moll/vim-bbye", -- Better buffer deletion
+		},
+		version = "*",
 		config = function()
-			require("bufferline").setup({
+			vim.o.mouse = "a"
+			vim.o.mousemoveevent = true
+
+			local bufferline = require("bufferline")
+
+			bufferline.setup({
 				options = {
 					mode = "buffers",
 					numbers = "none",
-					close_command = "bdelete %d",
-					diagnostics = "nvim_lsp",
-					separator_style = "thick",
+
+					separator_style = "thick", -- slant | thick | slope | thin
+					always_show_bufferline = true,
+
+					show_buffer_icons = true,
 					show_buffer_close_icons = true,
 					show_close_icon = false,
-					always_show_bufferline = true,
+					color_icons = true,
+					style_preset = bufferline.style_preset.no_italic,
+
+					close_command = "Bdelete! %d", -- using vim-bbye
+					right_mouse_command = "Bdelete! %d",
+
+					hover = {
+						enabled = true,
+						delay = 200,
+						reveal = { "close" },
+					},
+
+					diagnostics = "nvim_lsp",
+					diagnostics_update_in_insert = false,
+					diagnostics_indicator = function(count, level)
+						local icons = {
+							error = " ",
+							warning = " ",
+							info = " ",
+							hint = "󰌵 ",
+						}
+						return (icons[level] or "") .. count
+					end,
+
 					offsets = {
 						{
 							filetype = "neo-tree",
 							text = "Neo Tree",
 							text_align = "left",
+							-- highlight = "Directory",
 							-- separator = true,
 						},
 					},
 				},
+				-- highlights = {
+				-- 	fill = {
+				-- 		bg = "#181825", --  bufferline bar bg
+				-- 	},
+				-- },
 			})
 
-			vim.keymap.set("n", "<Tab>", "<cmd>BufferLineCycleNext<CR>")
-			vim.keymap.set("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>")
-			vim.keymap.set("n", "<leader>x", "<cmd>bdelete<CR>")
-
-			vim.keymap.set("n", "<leader>bl", function()
+			local map = vim.keymap.set
+			map("n", "<Tab>", "<cmd>BufferLineCycleNext<CR>", { silent = true, desc = "Next buffer" })
+			map("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>", { silent = true, desc = "Prev buffer" })
+			map("n", "<leader>bx", "<cmd>Bdelete<CR>", { desc = "Close buffer" })
+			map("n", "<leader>bl", function()
 				vim.o.showtabline = vim.o.showtabline == 0 and 2 or 0
 			end, { desc = "Toggle bufferline" })
 		end,
