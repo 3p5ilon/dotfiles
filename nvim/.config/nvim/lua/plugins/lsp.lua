801283b0230afcc60094d38e71671aa61695ba40
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2026-03-09 00:58:42 +0530

    add icons for diagnostics

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -62,7 +62,7 @@ return {
 				},
 				init_options = {
 					usePlaceholders = true,
-					completeUnimported = false,
+					completeUnimported = true,
 					clangdFileStatus = true,
 				},
 			})
@@ -107,7 +107,14 @@ return {
 			-- Diagnostics UI
 			vim.diagnostic.config({
 				virtual_text = true,
-				signs = true,
+				signs = {
+					text = {
+						[vim.diagnostic.severity.ERROR] = " ",
+						[vim.diagnostic.severity.WARN] = " ",
+						[vim.diagnostic.severity.HINT] = "󰌵 ",
+						[vim.diagnostic.severity.INFO] = " ",
+					},
+				},
 				underline = true,
 				update_in_insert = false,
 				severity_sort = true,
