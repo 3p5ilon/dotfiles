456de04ebbdc570ffbe81370183e71c50717462a
jiraiyahiddenleaf <jiraiyahiddenleaf27@gmail.com>
2026-03-15 00:17:23 +0530

    telescope kept for ui-select, keymaps commented out

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,37 +1,39 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    version = "*",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        build = "make",
-        cond = function()
-          return vim.fn.executable("make") == 1
-        end,
-      },
-      "nvim-telescope/telescope-ui-select.nvim", -- Makes LSP code actions appear in Telescope
-    },
-    config = function()
-      local telescope = require("telescope")
-      local builtin = require("telescope.builtin")
+	{
+		"nvim-telescope/telescope.nvim",
+		version = "*",
+		cmd = "Telescope",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				build = "make",
+				cond = function()
+					return vim.fn.executable("make") == 1
+				end,
+			},
+			"nvim-telescope/telescope-ui-select.nvim",
+		},
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
 
-      telescope.setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({})
-          },
-        },
-      })
-
-      pcall(telescope.load_extension, "fzf")
-      telescope.load_extension("ui-select")
-
-      vim.keymap.set("n", "<leader>ff", builtin.find_files)
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep)
-      vim.keymap.set("n", "<leader>fb", builtin.buffers)
-      vim.keymap.set("n", "<leader>fh", builtin.help_tags)
-    end,
-  },
+			pcall(require("telescope").load_extension, "fzf")
+			require("telescope").load_extension("ui-select") -- Makes LSP code actions appear in Telescope
+		end,
+		keys = {
+			-- Uncomment to use telescope instead of fzf-lua
+			-- { "<leader>tff", "<cmd>Telescope find_files<CR>",  desc = "Find files" },
+			-- { "<leader>tfg", "<cmd>Telescope live_grep<CR>",   desc = "Live grep" },
+			-- { "<leader>tfb", "<cmd>Telescope buffers<CR>",     desc = "Buffers" },
+			-- { "<leader>tfr", "<cmd>Telescope oldfiles<CR>",    desc = "Recent files" },
+			-- { "<leader>tfw", "<cmd>Telescope grep_string<CR>", desc = "Grep word" },
+			-- { "<leader>tfk", "<cmd>Telescope keymaps<CR>",     desc = "Keymaps" },
+		},
+	},
 }
