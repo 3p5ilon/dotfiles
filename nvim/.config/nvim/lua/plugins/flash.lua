5d8a41ed7f4e959c2b7ecf60dac83510523566e5
epsilon <jiraiyahiddenleaf27@gmail.com>
2026-08-20 00:38:48 +0530

    update flash conf

--- nvim/.config/nvim/lua/plugins/flash.lua
+++ nvim/.config/nvim/lua/plugins/flash.lua
@@ -1,15 +1,34 @@
 return {
-	{
-		"folke/flash.nvim",
-		event = "VeryLazy",
-		---@type Flash.Config
-		opts = {},
-		keys = {
-			{ "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-			{ "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-			{ "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-			{ "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-			{ "<C-s>", mode = "c", function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+	"folke/flash.nvim",
+	event = "VeryLazy",
+	---@type Flash.Config
+	opts = {
+		search = {
+			multi_window = false, -- Active window only
+			wrap = false, -- No buffer wrapping
 		},
+		jump = {
+			history = true, -- Save to jumplist (<C-o>)
+			autojump = false, -- Require explicit label press
+		},
+		label = {
+			uppercase = false, -- Lowercase labels only
+		},
+		modes = {
+			search = {
+				enabled = false, -- Keep native / search
+			},
+			char = {
+				enabled = true, -- Enhance f/F/t/T motions
+				autohide = true, -- Hide labels on fast typing
+			},
+		},
+	},
+	keys = {
+		{ "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash Jump" },
+		{ "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+		{ "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+		{ "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+		{ "<C-s>", mode = "c", function() require("flash").toggle() end, desc = "Toggle Flash Search" },
 	},
 }
