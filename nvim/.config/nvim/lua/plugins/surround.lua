722713bb29622110240e443518fdcb8f432e7176
epsilon <jiraiyahiddenleaf27@gmail.com>
2026-08-20 00:39:11 +0530

    update surround conf

--- nvim/.config/nvim/lua/plugins/surround.lua
+++ nvim/.config/nvim/lua/plugins/surround.lua
@@ -1,7 +1,19 @@
 return {
 	{
-		"kylechui/nvim-surround",
+		"echasnovski/mini.surround",
+		version = "*",
 		event = "VeryLazy",
-		opts = {},
+		opts = {
+			mappings = {
+				add = "gsa", -- Add surround (e.g., gsaiw")
+				delete = "gsd", -- Delete surround (e.g., gsd")
+				find = "gsf", -- Search forward for surround
+				find_left = "gsF", -- Search backward for surround
+				highlight = "gsh", -- Visually highlight target surround
+				replace = "gsr", -- Replace surround (e.g., gsr"')
+				update_n_lines = "gsn", -- Adjust line search distance
+			},
+			n_lines = 20, -- How many lines above/below to search for surrounds
+		},
 	},
 }
