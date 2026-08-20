f314b3951c4606b3056cba29e956a03c3b597a2a
epsilon <jiraiyahiddenleaf27@gmail.com>
2026-08-21 02:33:09 +0530

    update lualine config

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,68 +1,52 @@
 return {
 	{
 		"nvim-lualine/lualine.nvim",
-		config = function()
-			local style = "rounded" -- "sharp" | "rounded" | "soft"
+		opts = function()
+			local style = "rounded" -- "sharp" | "rounded" | "soft" | "slant"
 
 			local styles = {
-
 				sharp = {
 					component = { left = "", right = "" },
 					section = { left = "", right = "" },
 					edge = nil,
-					padding = {
-						a = { left = 2, right = 1 },
-						z = { left = 1, right = 2 },
-					},
+					padding = { a = { left = 2, right = 1 }, z = { left = 1, right = 2 } },
 				},
-
 				slant = {
-					-- component = { left = "", right = "" },
-					-- section = { left = "", right = "" },
-
 					component = { left = "", right = "" },
 					section = { left = "", right = "" },
+					-- component = { left = "", right = "" },
+					-- section = { left = "", right = "" },
 					edge = nil,
-					padding = {
-						a = { left = 2, right = 1 },
-						z = { left = 1, right = 2 },
-					},
+					padding = { a = { left = 2, right = 1 }, z = { left = 1, right = 2 } },
 				},
-
 				rounded = {
 					component = { left = "", right = "" },
 					section = { left = "", right = "" },
 					edge = { left = "", right = "" },
-					padding = {
-						a = { left = 1, right = 1 },
-						z = { left = 0, right = 1 },
-					},
+					padding = { a = { left = 1, right = 1 }, z = { left = 0, right = 1 } },
 				},
-
 				soft = {
 					component = { left = "", right = "" },
 					section = { left = "", right = "" },
 					edge = nil,
-					padding = {
-						a = { left = 2, right = 1 },
-						z = { left = 0, right = 2 },
-					},
+					padding = { a = { left = 2, right = 1 }, z = { left = 0, right = 2 } },
 				},
 			}
 
 			local s = styles[style]
 
 			local function edge_separator()
-				if not s.edge then
+				if not s or not s.edge then
 					return nil
 				end
 				return { left = s.edge.left, right = s.edge.right }
 			end
 
-			require("lualine").setup({
+			return {
 				options = {
 					theme = "auto",
 					icons_enabled = true,
+					globalstatus = true,
 					component_separators = s.component,
 					section_separators = s.section,
 					disabled_filetypes = {
@@ -71,7 +55,6 @@ return {
 				},
 
 				sections = {
-
 					lualine_a = {
 						{
 							"mode",
@@ -97,6 +80,33 @@ return {
 					},
 
 					lualine_x = {
+						-- -- Macro recording state
+						-- {
+						-- 	function()
+						-- 		return "%#Constant#" .. require("noice").api.status.mode.get()
+						-- 	end,
+						-- 	cond = function()
+						-- 		return package.loaded["noice"] and require("noice").api.status.mode.has()
+						-- 	end,
+						-- },
+						-- -- Command mode state
+						-- {
+						-- 	function()
+						-- 		return "%#Statement#" .. require("noice").api.status.command.get()
+						-- 	end,
+						-- 	cond = function()
+						-- 		return package.loaded["noice"] and require("noice").api.status.command.has()
+						-- 	end,
+						-- },
+						-- -- Search count
+						-- {
+						-- 	function()
+						-- 		return "%#DiagnosticWarn#" .. require("noice").api.status.search.get()
+						-- 	end,
+						-- 	cond = function()
+						-- 		return package.loaded["noice"] and require("noice").api.status.search.has()
+						-- 	end,
+						-- },
 						{
 							"diagnostics",
 							sources = { "nvim_diagnostic" },
@@ -104,8 +114,8 @@ return {
 								error = " ",
 								warn = " ",
 								info = " ",
-								-- hint  = " ",
 								hint = "󰌵 ",
+								-- hint  = " ",
 							},
 						},
 						-- "encoding",
@@ -125,7 +135,7 @@ return {
 				},
 
 				extensions = { "neo-tree", "oil", "lazy", "mason", "fzf" },
-			})
+			}
 		end,
 	},
 }
